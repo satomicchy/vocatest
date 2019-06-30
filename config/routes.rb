b2397ba7dfa469fa049323823470f5fa49d51497
satomicchy <satomi_t_sora@yahoo.co.jp>
2019-06-30 11:13:53 +0900

    add routes to guides controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'guides#index'
+  get 'guides/:book_id/:list_number', to: 'guides#word_list', as: 'guides_word_list'
+
   devise_for :users
   resources :words
   resources :books

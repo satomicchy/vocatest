286e26235e7555286e9b3153019c71a8de2137ad
satomicchy <satomi_t_sora@yahoo.co.jp>
2019-06-30 10:20:32 +0900

    add create User by devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_30_010250) do
+ActiveRecord::Schema.define(version: 2019_06_30_011409) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   create_table "words", force: :cascade do |t|
